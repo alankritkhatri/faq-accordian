367fbc86a39b50f4145410401dbec94e4372c186
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-04-05 12:46:38 +0530

    added functionality to open and close the FAQ answers

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //! Selecting Elements
-/* faq-open-icon */ const faqOpen = document.querySelectorAll(".faq-open");
-
+/* all faq-open-icons */ const faqOpen = document.querySelectorAll(".faq-open");
+/* all asnwers to faqs */ const answer = document.querySelectorAll(".answer");
 //! Event Listeners
 //? Adding the open faq feature
+console.log(faqOpen);
+faqOpen.forEach((icon, j) => {
+  return icon.addEventListener("click", function () {
+    answer[j].classList.toggle("block");
+  });
+});
